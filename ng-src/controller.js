f2b75ed83dd7b3b31f8b457d7832173f2d368eaa
Maorz30199 <cesarruiz@unicauca.edu.co>
2017-02-28 17:13:30 -0500

    Actualización
    Muestra los repositorios de una cuenta de git y se aplica una directiva
    personalizada para buscar y filtar un repositorio

--- ng-src/controller.js
+++ ng-src/controller.js
@@ -1,11 +1,52 @@
 angular.module("App",[])
-  .controller("controllerApp",function($scope,$http){
-
-    $http.get("https://api.github.com/users/Maorz30199/repos")
-      .then(function(response){
-        $scope.repos = response.data;
-      },
-      function(err){
-        console.log(err);
+.controller("controllerApp",function($scope,$http){
+  $scope.repos= [];
+  $http.get("https://api.github.com/users/Maorz30199/repos")
+    .then(function(response){
+      $scope.posts = response.data;
+      for(var i= response.data.length -1; i>= 0; i--){
+        var repo = response.data[i];
+        $scope.repos.push(repo.name);
+        console.log(response.data);
+      };
+    },
+    function(err){
+      console.log(err);
+    });
+    $scope.optionSelected = function(data){
+      $scope.$apply(function(){
+        $scope.main_repo = data;
+      })
+    }
+})
+  .directive('myAutocomplete',function(){
+    function Link(scope,element,attrs){
+      $(element).autocomplete({
+        source: scope.$eval(attrs.myAutocomplete),
+        select: function(ev,ui){
+          ev.preventDefault();
+          if(ui.item){
+            scope.optionSelected(ui.item.value);
+          }
+        },
+        focus: function(ev,ui){
+          ev.preventDefault();
+          $(this).val(ui.item.label);
+        }
       });
-  });
+    };
+    return{
+      link: Link
+    };
+  })
+  .directive('backImg',function(){
+    return function(scope,element,attrs){
+      attrs.$observe('backImg',function(value){
+        element.css({
+          'background': "url('"+value+"')",
+          'background-size': 'cover',
+          'background-position': 'center'
+        });
+      });
+    };
+});
